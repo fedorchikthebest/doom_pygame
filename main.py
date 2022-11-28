c0e1d03b3ae7b90e2d10c20bc7a512fc9f518451
gimonchik <73658246+fedorchikthebest@users.noreply.github.com>
2022-11-28 16:43:56 +0300

    Update main.py
    beta 1.2

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 def rng(p_x, p_y, p_a):
     i = 0
     while i < 500:
-        y = p_x + i * math.sin(p_a / 360 * 2 * math.pi)
-        x = p_y + i * math.cos(p_a / 360 * 2 * math.pi)
+        y = p_y + i * math.sin(p_a / 360 * 2 * math.pi)
+        x = p_x + i * math.cos(p_a / 360 * 2 * math.pi)
         if MAP[int(y) // TILE_SIZE_Y][int(x) // TILE_SIZE_X] == '#':
             break
         i += 1
@@ -44,11 +44,27 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     TILE_SIZE_X = width // len(MAP[0])
     TILE_SIZE_Y = height // len(MAP)
-    while pygame.event.wait().type != pygame.QUIT:
+    p_x = 400
+    p_y = 400
+    fps = 60
+    running = True
+    while running:
         screen.fill((0, 0, 0))
         old_time = time.time()
         draw_map()
-        for i in range(0, 180, 1):
-            pygame.draw.line(screen, (0, 255, 0), (400, 400), rng(400, 400, i), 3)
-        pygame.draw.circle(screen, (255, 0, 0), (400, 400), 3)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    p_y -= 10
+                if event.key == pygame.K_a:
+                    p_x -= 10
+                if event.key == pygame.K_s:
+                    p_y += 10
+                if event.key == pygame.K_d:
+                    p_x += 10
+            if event.type == pygame.QUIT:
+                running = False
+        for i in range(0, 90, 1):
+            pygame.draw.line(screen, (0, 255, 0), (p_x, p_y), rng(p_x, p_y, i), 1)
+        pygame.draw.circle(screen, (255, 0, 0), (p_x, p_y), 3)
         pygame.display.flip()
