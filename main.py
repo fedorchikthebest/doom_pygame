5b2bd9172014f285d064025f40da9a7f528bc55e
gimonchik <73658246+fedorchikthebest@users.noreply.github.com>
2022-11-28 15:48:09 +0300

    Update main.py
    alpha 1.0

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import pygame
-import sys
 import math
 import time
-
-SINS = {}
-COSS = {}
-
-i = 0
-while i < 360:
-    SINS[str(float(i))] = math.sin(i)
-    i += 0.1
-i = 0
-
-i = 0
-while i < 360:
-    COSS[str(float(i))] = math.cos(i)
-    i += 0.1
-i = 0
 
 MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
        ['#', '1', '2', '3', '1', '1', '2', '3', '#', '1', '2', '#'],
@@ -35,8 +19,8 @@ MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 def rng(p_x, p_y, p_a):
     i = 0
     while i < 500:
-        x = p_x - i * math.sin(p_a)
-        y = p_y + i * math.cos(p_a)
+        y = p_x + i * math.sin(p_a / 360 * 2 * math.pi)
+        x = p_y + i * math.cos(p_a / 360 * 2 * math.pi)
         if MAP[int(y) // TILE_SIZE_Y][int(x) // TILE_SIZE_X] == '#':
             break
         i += 1
@@ -56,7 +40,7 @@ a = 0
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 800, 800
+    size = width, height = 960, 960
     screen = pygame.display.set_mode(size)
     TILE_SIZE_X = width // len(MAP[0])
     TILE_SIZE_Y = height // len(MAP)
@@ -64,6 +48,7 @@ if __name__ == '__main__':
         screen.fill((0, 0, 0))
         old_time = time.time()
         draw_map()
-        for i in range(-45, 45):
-            pygame.draw.line(screen, (0, 255, 0), (400, 400), rng(400, 400, 7))
+        for i in range(0, 180, 1):
+            pygame.draw.line(screen, (0, 255, 0), (400, 400), rng(400, 400, i), 3)
+        pygame.draw.circle(screen, (255, 0, 0), (400, 400), 3)
         pygame.display.flip()
